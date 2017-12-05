7d7ba125f2e324b3e9952b80072c36beb4f555e6
Hana Johnson <zencodecat@gmail.com>
2017-12-04 22:08:06 -0600

    fixing class names in footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import '../font-awesome-4.7.0/css/font-awesome.css';
 import '../css/footer.css';
 
@@ -9,16 +8,16 @@ const Footer = ({name, position}) => {
 			<footer id="portfolio-footer">
 				<nav id="contact-nav-footer">
 					<a href="mailto:zencodecat@gmail.com" id="email-icon">
-						<i class="fa fa-envelope" aria-hidden="true" />
+						<i className="fa fa-envelope" aria-hidden="true" />
 					</a>
 					<a href="https://github.com/hanabeth" id="github-icon">
-						<i class="fa fa-github" aria-hidden="true" />
+						<i className="fa fa-github" aria-hidden="true" />
 					</a>
 					<a href="https://www.linkedin.com/in/hanabeth/" id="linkedin-icon">
-						<i class="fa fa-linkedin" aria-hidden="true" />
+						<i className="fa fa-linkedin" aria-hidden="true" />
 					</a>
 					<a href="https://www.instagram.com/zencodecat/" id="instagram-icon">
-						<i class="fa fa-instagram" aria-hidden="true" />
+						<i className="fa fa-instagram" aria-hidden="true" />
 					</a>
 				</nav>
 				<p id="footer-copyright">
